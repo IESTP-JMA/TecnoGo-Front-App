5f4018f4034ff56076bf2f12a4b9f8dd2f72cbb4
Y3R41D <pomayerald11@gmail.com>
2024-10-24 17:43:47 -0500

    improve header

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,139 +1,71 @@
 // app/(tabs)/_layout.jsx
-import { View, Text, Image, StatusBar, Pressable } from "react-native";
+import { Text, Image, StatusBar, Pressable } from "react-native";
 import { Tabs, useRouter } from "expo-router";
 import {
   PersonIcon,
   BellIcon,
-  CheckIcon,
-  EditIcon,
   HomeIcon,
   HomeIconOutline,
 } from "../../components/Icons";
 import { useAuth } from "../contexts/AuthContext";
-import {
-  EditProfileProvider,
-  useEditProfileContext,
-} from "../contexts/EditProfileContext";
-
-function ProfileHeaderRight() {
-  const { isEditing, setIsEditing } = useEditProfileContext();
-  const { userData } = useAuth();
-
-  const handleSaveProfile = async () => {
-    try {
-      console.log(userData);
-      setIsEditing(false);
-
-      //   const response = await fetch("TU_URL_API/profile", {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify(profileData),
-      //   });
-      //   if (response.ok) {
-      //     setIsEditing(false);
-      //     // Opcional: Mostrar mensaje de éxito
-      //   } else {
-      //     // Manejar error
-      //     console.error("Error al guardar el perfil");
-      //   }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
-  return (
-    <Pressable
-      className="mr-3"
-      onPress={() => {
-        if (isEditing) {
-          handleSaveProfile();
-        } else {
-          setIsEditing(true);
-        }
-      }}
-    >
-      {isEditing ? (
-        <View className="flex-row items-center gap-x-1">
-          <CheckIcon />
-          <Text className="text-emerald-800 font-SenMedium ">Guardar</Text>
-        </View>
-      ) : (
-        <EditIcon />
-      )}
-    </Pressable>
-  );
-}
 
 export default function TabsLayout() {
   const router = useRouter();
   const { userData } = useAuth();
-  const visibleTabs = ["home/index", "profile"];
-
-  //   const handleSaveProfile = async () => {
-  //     try {
-  //       const response = await fetch("TU_URL_API/profile", {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(profileData),
-  //       });
-
-  //       if (response.ok) {
-  //         setIsEditing(false);
-  //         // Opcional: Mostrar mensaje de éxito
-  //       } else {
-  //         // Manejar error
-  //         console.error("Error al guardar el perfil");
-  //       }
-  //     } catch (error) {
-  //       console.error("Error:", error);
-  //     }
-  //   };
 
   return (
-    <EditProfileProvider>
+    <>
       <StatusBar backgroundColor="#064E3B" barStyle="light-content" />
       <Tabs
-        screenOptions={({ route }) => ({
+        screenOptions={{
           tabBarActiveTintColor: "#065F46",
-          tabBarButton: (props) => {
-            if (visibleTabs.includes(route.name)) {
-              return <Pressable {...props} />;
-            }
+          headerTintColor: "#065F46",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontFamily: "SenBold",
+            color: "#064E3B",
+            fontSize: 23,
           },
-        })}
+          tabBarLabelStyle: {
+            fontFamily: "SenMedium",
+            fontSize: 13,
+          },
+        }}
       >
         <Tabs.Screen
           name="home/index"
           options={{
-            headerTitle: "",
-            headerStyle: { backgroundColor: "#064E3B" },
+            headerStyle: {
+              backgroundColor: "#064E3B",
+              height: 95,
+            },
             headerLeft: () => (
               <Pressable
-                className="flex-row gap-x-2 pl-2"
+                className="pl-2"
                 onPress={() => router.push("profile")}
               >
                 <Image
-                  className="rounded-full w-12 h-12"
+                  className="rounded-full w-14 h-14"
                   source={require("../../assets/avatar.png")}
                 />
-                <View>
-                  <Text className="text-white text-xl font-SenMedium">
-                    Hola, {userData.firstNames} 👋
-                  </Text>
-                  <Text className="text-zinc-200 font-SenRegular pl-1">
-                    Bienvenida
-                  </Text>
-                </View>
               </Pressable>
             ),
+            headerTitleAlign: "left",
+            headerTitle: () => (
+              <>
+                <Text className="text-white text-xl font-SenMedium -ml-1">
+                  Hola, {userData.firstNames} 👋
+                </Text>
+                <Text className="text-zinc-200 font-SenRegular -ml-1">
+                  Bienvenida
+                </Text>
+              </>
+            ),
+
             headerRight: () => (
-              <View className="mr-2">
+              <Pressable className="p-2.5  mr-1">
                 <BellIcon />
-              </View>
+              </Pressable>
             ),
             tabBarIcon: ({ size, color, focused }) => {
               if (focused) {
@@ -143,45 +75,24 @@ export default function TabsLayout() {
               }
             },
             tabBarLabel: "Inicio",
-            tabBarLabelStyle: { fontFamily: "SenMedium", fontSize: 12 },
           }}
-          //   listeners={{
-          //     tabPress: () => setIsEditing(false),
-          //   }}
         />
         <Tabs.Screen
           name="profile"
           options={{
             title: "Mi Perfil",
             headerTitle: "Mi Perfil",
-            headerTitleStyle: {
-              fontFamily: "SenSemiBold",
-              color: "#064E3B",
-              fontSize: 20,
-            },
-            headerTitleAlign: "center",
             headerStyle: { backgroundColor: "#E6F2EC" },
-            // headerLeft: () => {
-            //   <Pressable
-            //     className="ml-3 flex-row items-center"
-            //     onPress={() => router.back()}
-            //   >
-            //     <ChevronLeftIcon color="#064E3B" size={20} />
-            //     <Text className="font-SenRegular text-base"> volver</Text>
-            //   </Pressable>
-            // },
-            headerRight: () => <ProfileHeaderRight />,
             tabBarIcon: ({ color, focused, size }) => (
               <PersonIcon size={size} color={color} filled={focused} />
             ),
             tabBarLabel: "Mi Perfil",
-            tabBarLabelStyle: { fontFamily: "SenMedium", fontSize: 12 },
           }}
           //   listeners={{
           //     tabPress: () => setIsEditing(false),
           //   }}
         />
       </Tabs>
-    </EditProfileProvider>
+    </>
   );
 }
