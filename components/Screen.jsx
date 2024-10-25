60a96bc915eba8a172d91237404806b2a399d471
Y3R41D <pomayerald11@gmail.com>
2024-10-25 04:03:10 -0500

    del

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
 
 export default function Screen({ children }) {
-  return <View className="flex-1 bg-gray-500">{children}</View>;
+  return <View className="flex-1 bg-[#E6F2EC] p-4">{children}</View>;
 }
--- components/form/IconWithLabel.jsx
+++
@@ -1,2 +0,0 @@
-import { Text, View } from "react-native";
-import { User } from "lucide-react-native";
