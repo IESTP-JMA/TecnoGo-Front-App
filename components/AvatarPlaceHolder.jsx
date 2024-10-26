9242aa5c253cbdcb4831a6d8aa59117538476ba4
Y3R41D <pomayerald11@gmail.com>
2024-10-26 18:03:25 -0500

    add Avatar Placeholder

---
+++ components/AvatarPlaceHolder.jsx
@@ -0,0 +1,27 @@
+import { Text, View } from "react-native";
+import { useAuth } from "../app/contexts/AuthContext";
+
+export default function AvatarPlaceHolder({
+  customStyle,
+  customClass,
+  customTextStyle,
+  customTextClass,
+  size,
+}) {
+  const { userData } = useAuth();
+
+  return (
+    <View
+      style={customStyle}
+      className={`bg-gray-300 rounded-full w-${size} h-${size} items-center justify-center ${customClass}`}
+    >
+      <Text
+        style={customTextStyle}
+        className={`${customTextClass} font-SenBold text-gray-600`}
+      >
+        {userData.firstNames[0].toUpperCase() +
+          userData.lastNames[0].toUpperCase()}
+      </Text>
+    </View>
+  );
+}
